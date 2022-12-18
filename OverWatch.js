3f3f654475d9c543cd324b66f8aab2c576b87fe6
Shahnur Alam <codename065@users.noreply.github.com>
2022-12-18 12:20:30 +0600

    Create OverWatch.js

---
+++ OverWatch.js
@@ -0,0 +1,26 @@
+class OverWatch {
+        constructor (ID)  {
+            this.ID = ID;
+            let ow = this;
+            this.data = new Proxy({}, {
+                set(target, name, value) {
+                    target[name] = value;
+                    if(typeof value === 'string')
+                        document.body.innerHTML = document.body.innerHTML.replace(`{${this.ID}.${name}}`, value);
+                    if(typeof value === 'object'){
+                        ow.process(name, value);
+                    }
+                }
+            });
+        }
+        process(name, obj) {
+            for (const [key, value] of Object.entries(obj)) {
+                let index = `${name}.${key}`;
+                if(typeof value === 'string') {
+                    document.getElementById(this.ID).innerHTML = document.getElementById(this.ID).innerHTML.replace(`{${index}}`, value);
+                } else if(typeof value === 'object'){
+                    this.process(index, value);
+                }
+            }
+        }
+    }
