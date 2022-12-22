ceb4c25fde654ed7e4233218bab9817510a756d8
Shahnur Alam <codename065@users.noreply.github.com>
2022-12-22 12:34:39 +0600

    Update OverWatch.js

--- OverWatch.js
+++ OverWatch.js
@@ -6,26 +6,38 @@ const OverWatch = (ID) => {
         const buffer_html = area.innerHTML.replaceAll(/{(.+)}/ig, "<data name='$1'>{$1}</data>");
         area.innerHTML = buffer_html;
         let _loops = [];
+        let _attrs = [];
         let looped_sections = document.querySelectorAll(`#${ID} loop`);
+        let attrs = document.querySelectorAll(`#${ID} [w-attr]`);
+
+        for (const [key, elm] of Object.entries(attrs)) {
+            let _attr = elm.getAttribute("w-attr");
+            let _var = elm.getAttribute('w-val');
+            if(!_attrs[_var]) _attrs[_var] = []
+            _attrs[_var].push({_attr: _attr, _var: _var, _elm: elm});
+        }
         for (const [key, loop] of Object.entries(looped_sections)) {
             let _for = loop.getAttribute(":for");
             let _each = loop.getAttribute(":each");
             _loops[_for] = {_for: _for, _each: _each, _repeat: loop.innerHTML, _loop: loop};
             loop.innerHTML = '';
         }
-        console.log('_loops', _loops);
+        //console.log('_attrs', _attrs);
 
-        let parent = [];        
+        let parent = [];
+        //let data_source = {};
         let flat_data = [];
         let key = '';
 
         const handler = {
 
             set(target, prop, value) {
-                key = parent.join('.') + `.${prop}`;                
+                key = parent.join('.') + `.${prop}`;
+                //console.log('Called for ' + "" + key);
                 parent = [];
                 if (typeof value === 'object') {
-                    value = new Proxy(value, handler);                  
+                    value = new Proxy(value, handler);
+                    //console.log('key:', key);
                     process(key, value);
                 }
                 Reflect.set(target, prop, value);
@@ -43,6 +55,12 @@ const OverWatch = (ID) => {
             document.querySelectorAll(`#${ID} data[name='${key}']`).forEach(elm => {
                 elm.innerHTML = value;
             });
+            if(_attrs[key]) {
+                let elms = _attrs[key];
+                elms.forEach((elm) => {
+                   elm._elm.setAttribute(elm._attr, value);
+                });
+            }
         };
 
         const valueof = (data_source, index) => {
@@ -50,12 +68,13 @@ const OverWatch = (ID) => {
             index = index.split('.');
             index.forEach(prop => {
                 value = Reflect.get(data_source, prop);
-            });           
+            });
+            //console.log('vo ', value);
             return value;
         }
 
         const process = (name, obj) => {
-            console.log('Processed ' + name, obj);
+            //console.log('Processed ' + name, obj);
             if (_loops[name]) {
                 let loop = _loops[name];
                 let _for = obj;
@@ -70,7 +89,8 @@ const OverWatch = (ID) => {
                     _html += _processed_html;
                     index++;
                 });
-                loop._loop.innerHTML = _html;                
+                loop._loop.innerHTML = _html;
+                //console.log(_html)
             }
 
             for (let [key, value] of Object.entries(obj)) {
